387ec49d9f1789cafa6dffa424fb98836745ce94
Gubochka <newicraft2201@gmail.com>
2023-03-08 22:36:07 +0200

    Added task3 for Lesson1

---
+++ Lesson1/Task3/task3.cs
@@ -0,0 +1,19 @@
+﻿const int listSize = 10;
+List<int> numbersList = new List<int>();
+
+for (int i = 0; i < listSize; i++) {
+    Console.Write($"Enter {i+1} number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbersList.Add(number);
+}
+
+for (int i = 0; i < listSize; i++) {
+    int numberCount = numbersList.Count(num => {
+        return num == numbersList[i];
+    });
+    if (numberCount > 1 && numbersList.LastIndexOf(numbersList[i]) <= 9)
+        numbersList.Add(numbersList[i]);
+}
+
+foreach (int n in numbersList) 
+    Console.Write($"{n}, ");
