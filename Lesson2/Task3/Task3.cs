ce98e9a1a7a714a3c6d06f63c63ee902de820c2f
Gubochka <newicraft2201@gmail.com>
2023-03-28 21:12:39 +0300

    Remake Task 3 for Lesson 2

--- Lesson2/Task3/Task3.cs
+++ Lesson2/Task3/Task3.cs
@@ -11,24 +11,30 @@
     new Customer("Harry", 44, "Address10")
 };
 Console.WriteLine(">>>List of customers:");
-foreach(var customer in customersList) {
+foreach (var customer in customersList)
+{
     Console.WriteLine($"> {customer.name} - {customer.age} years - {customer.address}");
 }
 Console.Write("\n\n>>>Enter the name of the required customers: ");
 var name = Console.ReadLine();
 
 Console.WriteLine("\n>>>Selected customers:");
-var selectedCustomers = from c in customersList where c.name == name && c.age > 18 select c;
-foreach (var customer in selectedCustomers) {
+var selectedCustomers = from c in customersList where c.name == name select c;
+foreach (var customer in selectedCustomers)
+{
     Console.WriteLine($"> {customer.name} - {customer.age} years - {customer.address}");
 }
 
+Console.WriteLine($"> Quantity customers over 18 years old: {(from c in selectedCustomers where c.age > 18 select c).ToList().Count}");
 
-class Customer {
+
+class Customer
+{
     public string name;
     public int age;
     public string address;
-    public Customer(string name, int age, string address) {
+    public Customer(string name, int age, string address)
+    {
         this.name = name;
         this.age = age;
         this.address = address;
