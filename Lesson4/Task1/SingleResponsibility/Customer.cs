56a1b7908752efeaf781165a44c9fcf1b6a426cb
Gubochka <newicraft2201@gmail.com>
2023-03-30 21:59:07 +0300

    Added task 1 for Lesson 4

---
+++ Lesson4/Task1/SingleResponsibility/Customer.cs
@@ -0,0 +1,32 @@
+﻿namespace SingleResponsibility
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private decimal Balance { get; set; }
+
+        public Customer(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+
+        public void SaveToDatabase()
+        {
+            Console.WriteLine("Saved!");
+        }
+
+        public void UpdateBalance(decimal newBalance)
+        {
+            Balance = newBalance;
+            SaveToDatabase();
+        }
+    }
+}
