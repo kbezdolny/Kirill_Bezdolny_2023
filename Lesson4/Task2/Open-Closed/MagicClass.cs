adddac953459474a8a0c17e9750d9df4b58bf91e
Gubochka <newicraft2201@gmail.com>
2023-04-07 18:26:00 +0300

    Changed MagicClass system

--- Lesson4/Task2/Open-Closed/MagicClass.cs
+++ Lesson4/Task2/Open-Closed/MagicClass.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Open_Closed;
 
-namespace Open_Closed
+public class MagicClass
 {
-    public class MagicClass
+    public virtual void CountYourMagic(int magic)
     {
-        public virtual void CountYourMagic(int magic)
-        {
-            if (magic == 150)
-            {
-                Console.WriteLine("Wow, your magic is fire magic!");
-                return;
-            }
-            if (magic == 50000000)
-            {
-                Console.WriteLine("Incredible! You have 50 millions of power! It's water magic!");
-                return;
-            }
-            Console.WriteLine("I understand you...");
-        }
+        Console.WriteLine("I understand you...");
     }
 }
---
+++ Lesson4/Task2/Open-Closed/NewMagic.cs
@@ -0,0 +1,18 @@
+﻿namespace Open_Closed;
+
+public class NewMagic : MagicClass
+{
+    public override void CountYourMagic(int magic)
+    {
+        if (magic == 150)
+        {
+            Console.WriteLine("Wow, your magic is fire magic!");
+            return;
+        }
+        if (magic == 50000000)
+        {
+            Console.WriteLine("Incredible! You have 50 millions of power! It's water magic!");
+            return;
+        }
+    }
+}
